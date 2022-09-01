7525cec4d9289f16d04301060c2956f736583a88
Jørgen Dyrskog <joergen.dyrskog@gmail.com>
2022-09-01 13:15:40 +0200

    add node1 project

---
+++ node1/main.c
@@ -0,0 +1,18 @@
+/*
+ * node1.c
+ *
+ * Created: 01.09.2022 13:09:46
+ * Author : joerg
+ */ 
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
