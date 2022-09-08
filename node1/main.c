39b85d30d56a404bb9c88dc2a5559a4d1b8fd1b4
linehoyb <63612725+linehoyb@users.noreply.github.com>
2022-09-08 13:25:54 +0200

    Implenent UART and printf

--- node1/main.c
+++ node1/main.c
@@ -5,51 +5,69 @@
  * Author : joerg
  */ 
 
-#include <avr/io.h>
-
-#define FOSC 4915200// Clock Speed
+#define F_CPU 4915200
 #define BAUD 9600
-#define BAUDRATE FOSC/16/BAUD-1
 
-void usart_setup()
-{
-	UBRR1H = (uint8_t)(BAUDRATE >> 8); // Set baud rate
-	UBRR1L = (uint8_t)BAUDRATE;
-	/* Enable receiver and transmitter */
-	UCSR1B = (1<<RXEN1)|(1<<TXEN1);
-	/* Set frame format: 8 bit data, 2 stop bit */
-	UCSR1C = (1<<URSEL1)|(1<<USBS1)|(3<<UCSZ01);
-}
+#include <stdio.h>
+#include <avr/io.h>
+#include "util/delay.h"
 
-
-void send_character(uint8_t data)
+int send_character(char data, FILE * file)
 {
 	// Wait for available uart to send
 	while (!(UCSR1A & (1 << UDRE1)));
 	// Send data
 	UDR1 = data;
+	return 0;
 }
 
-
-uint8_t receive_character()
+int receive_character(FILE * file)
 {
 	// Wait for available data
 	while (!(UCSR1A & (1 << RXC1)));
 	return UDR1;
 }
 
+void usart_setup()
+{
+	unsigned long baud = BAUD;
+	unsigned long clockspeed = F_CPU;
+	unsigned long ubrr = clockspeed/16/baud-1;
+	
+	UBRR1H = (uint8_t)(ubrr >> 8); // Set baud rate
+	UBRR1L = (uint8_t)ubrr;
+	/* Enable receiver and transmitter */
+	UCSR1B = (1<<RXEN1)|(1<<TXEN1);
+	/* Set frame format: 8 bit data, 2 stop bit */
+	UCSR1C = (1<<URSEL1)|(1<<USBS1)|(1<<UCSZ10)|(1<<UCSZ11);
+	
+	/*Enable printf */
+	fdevopen (send_character, receive_character);
+}
 
 int main(void)
-{
-	/* Replace with your application code */
+{	
 	usart_setup();
-	send_character('f');
-	uint8_t rx_data;
+
 	while (1)
 	{
-		rx_data = receive_character;
-		send_character(rx_data);
+		_delay_ms(500);
+		printf("HELLO WORLD \r\n");
 	}
 }
 
-
+// OLD SEND/RECIEVE FUNCTIONS WITHOUT PRINTF
+// void send_character(uint8_t data)
+// {
+// 	// Wait for available uart to send
+// 	while (!(UCSR1A & (1 << UDRE1)));
+// 	// Send data
+// 	UDR1 = data;
+// }
+//
+// uint8_t receive_character()
+// {
+// 	// Wait for available data
+// 	while (!(UCSR1A & (1 << RXC1)));
+// 	return UDR1;
+// }
