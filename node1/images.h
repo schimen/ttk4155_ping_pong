0de44e2dac3ac24a4a42b86a8276ea2709bd55d0
schimen <einesimen@gmail.com>
2022-09-29 16:22:40 +0200

    Add images

---
+++ node1/images.h
@@ -0,0 +1,19 @@
+/*
+ * fonts.h
+ *
+ * Library of fonts
+ * Large: 8x8, normal: 5x7 and small: 4x7
+ */
+#ifndef IMAGES_H_
+#define IMAGES_H_
+
+#include <avr/pgmspace.h>
+
+const unsigned char PROGMEM trollg[64][8];
+
+const unsigned char PROGMEM parrot[64][8];
+
+const unsigned char PROGMEM skyline[91][8];
+
+
+#endif
