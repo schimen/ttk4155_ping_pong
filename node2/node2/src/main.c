84b282e2e3e7ef41da6a8456f620c71a9ede8f1e
Jørgen Dyrskog <joergen.dyrskog@gmail.com>
2022-10-20 10:13:12 +0200

    add node2

---
+++ node2/node2/src/main.c
@@ -0,0 +1,21 @@
+/*
+ * node2.c
+ *
+ * Created: 20.10.2022 10:02:51
+ * Author : joerg
+ */ 
+
+
+#include "sam.h"
+
+
+int main(void)
+{
+    /* Initialize the SAM system */
+    SystemInit();
+
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
