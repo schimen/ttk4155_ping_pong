f6e8ba842879592a059cb96f0a2520f550f375ca
schimen <einesimen@gmail.com>
2022-11-03 14:09:18 +0100

    Add servo pos function

--- node2/include/pwm_lib.c
+++ node2/include/pwm_lib.c
@@ -8,67 +8,64 @@
 #include "pwm_lib.h"
 
 int pwm_setup(){
-	// Sets up PWM on pin PC18, PWM channel 6H
-	
 	// Enable PWM in PMC and wait for register to be set
 	PMC->PMC_PCER1 |= PMC_PCER1_PID36;
 	while((PMC->PMC_PCSR1 & PMC_PCSR1_PID36) == 0);
-
 	// Disable PIOC18
 	PIOC->PIO_PDR |= PIO_PDR_P18;
-	
 	// Choose peripheral B
 	PIOC->PIO_ABSR |= PIO_ABSR_P18;
-	
 	// Disable PWM write protect for register group 0, 1, 2, 3, 4 and 5
 	PWM->PWM_WPCR = (PWM_WPCR_WPCMD(0) | (0x3F << 1));
-
 	// Disable PWM channel 6
 	PWM->PWM_DIS = (1 << 6);
-	
 	// PWM clock config
 	// clock A prescaler X = 128
 	PWM->PWM_CLK = (PWM_CLK_DIVA(1) | PWM_CLK_PREA(0x07));
-	
-	// Choose clock A for channel 6
-	PWM->PWM_CH_NUM[6].PWM_CMR = PWM_CMR_CPRE_CLKA;
-	
+	// Choose clock A for channel 6 and invert output
+	PWM->PWM_CH_NUM[6].PWM_CMR = PWM_CMR_CPRE_CLKA | PWM_CMR_CPOL;
 	// Set sync mode
 	PWM->PWM_SCM |= PWM_SCM_UPDM_MODE0;
-	
 	// Set Channel period
 	// X = 128, CPRD = 13125
 	PWM->PWM_CH_NUM[6].PWM_CPRD = PWM_CPRD_CPRD(0x3345);
-	
 	// Set channel duty cycle
 	PWM->PWM_CH_NUM[6].PWM_CDTY = PWM_CDTY_CDTY(0);
-	
 	// Enable PWM Channel 6
 	PWM->PWM_ENA = (1 << 6);
-	
 	// Wait for PWM channel enable
 	while((PWM->PWM_SR & (1 << 6)) == 0);
-	
-	if (PWM->PWM_SR & (1 << 6)){
-		printf("PWM 6 enabled\n\r");
-		return 0;
-	}
-	else{
-		printf("PWM 6 not enabled, %d \n\r", (PWM->PWM_SR & 0xFF));
-		return -1;
-	}
+	// Return 0 (success) when PWM has been enabled
+	return 0;
 }
 
+// Set the pulse value of the PWM
+static inline void pwm_set_pulse(uint32_t pulse, uint8_t channel) {
+	// Set CDTYUPD register to update PWM pulse
+	PWM->PWM_CH_NUM[channel].PWM_CDTYUPD = PWM_CDTY_CDTY(pulse);
+	// Trigger duty cycle update on next period
+	PWM->PWM_SCUC = PWM_SCUC_UPDULOCK;
+}
 
-void pwm_set_duty_cycle(uint8_t duty_cycle, uint8_t channel){
+void pwm_set_duty_cycle(uint8_t duty_cycle, uint8_t channel) {
 	// duty cycle in percentage
 	if (duty_cycle > 100) {
 		duty_cycle = 100;
 	}
 	uint32_t cprd = PWM->PWM_CH_NUM[channel].PWM_CPRD;
-	uint32_t dc_cdty = (duty_cycle*cprd)/100;
-	PWM->PWM_CH_NUM[channel].PWM_CDTYUPD = PWM_CDTY_CDTY(dc_cdty);
-	
-	// Trigger duty cycle update on next period
-	PWM->PWM_SCUC = PWM_SCUC_UPDULOCK;
+	uint32_t pulse = (duty_cycle*cprd)/100;
+	pwm_set_pulse(pulse, channel);
+}
+
+void servo_set_pos(uint8_t pos, uint8_t channel) {
+	// duty cycle in percentage
+	if (pos > 180) {
+		pos = 180;
+	}
+	uint32_t cprd = PWM->PWM_CH_NUM[channel].PWM_CPRD;
+	uint32_t cprd_top = cprd/10;
+	uint32_t cprd_bottom = cprd_top/2;
+	uint32_t cprd_range = cprd_top - cprd_bottom;
+	uint32_t pulse = ((pos*cprd_range)/180) + cprd_bottom;
+	pwm_set_pulse(pulse, channel);
 }
--- node2/include/pwm_lib.h
+++ node2/include/pwm_lib.h
@@ -12,8 +12,29 @@
 #include "sam.h"
 #include <stdio.h>
 
+/**
+ * @brief Set up PWM on pin PC18, PWM channel 6H
+ * 
+ * @return int 0 if succesfull, negative value on error
+ */
 int pwm_setup();
+
+/**
+ * @brief Set duty cycle for pwm
+ * 
+ * @param duty_cycle Unsigned number between 0 and 100 
+ *   representing percentage of duty-cycle to set
+ * @param channel PWM channel
+ */
 void pwm_set_duty_cycle(uint8_t duty_cycle, uint8_t channel);
 
+
+/**
+ * @brief Set servo position
+ * 
+ * @param pos Unsigned number between 0 and 180 representing servo position
+ * @param channel PWM channel
+ */
+void servo_set_pos(uint8_t pos, uint8_t channel);
 
 #endif /* PWM_H_ */
